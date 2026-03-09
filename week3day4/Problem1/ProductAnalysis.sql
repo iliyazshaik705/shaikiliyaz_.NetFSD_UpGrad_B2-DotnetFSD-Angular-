8680dd5896f37bc04b93f9ed53e4dc7b66beb38d
Shaik Iliyaz <iliyazshaik705@gmail.com>
2026-03-09 09:18:06 +0530

    Add files via upload

---
+++ week3day4/Problem1/ProductAnalysis.sql
@@ -0,0 +1,15 @@
+SELECT 
+    CONCAT(product_name, ' (', model_year, ')') AS product_details,
+    list_price,
+    (SELECT AVG(list_price) 
+     FROM products p2 
+     WHERE p2.category_id = p1.category_id) AS category_avg_price,
+    list_price - (SELECT AVG(list_price) 
+                  FROM products p2 
+                  WHERE p2.category_id = p1.category_id) AS price_difference
+FROM products p1
+WHERE list_price > (
+        SELECT AVG(list_price)
+        FROM products p2
+        WHERE p2.category_id = p1.category_id
+);
